8ee85531399e38e7c309045d788f370a45390587
supathida <supathidas61@nu.ac.th>
2020-07-29 19:49:34 +0700

    folder perpared

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueResource from 'vue-resource'
 
 Vue.config.productionTip = false
+
+Vue.use(VueResource)
 
 /* eslint-disable no-new */
 new Vue({
